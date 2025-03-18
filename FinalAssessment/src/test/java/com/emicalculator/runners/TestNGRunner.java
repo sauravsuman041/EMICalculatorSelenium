5ce49cf1749a9b7143dcd3a6b1d3e0caa34ff984
b59user21 <b59user21@rpsconsulting.in>
2025-03-18 19:13:50 +0530

    Initial commit

--- FinalAssessment/src/test/java/com/emicalculator/runners/TestNGRunner.java
+++ FinalAssessment/src/test/java/com/emicalculator/runners/TestNGRunner.java
@@ -9,7 +9,14 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		
 		features = "src/test/resources/features/loan.feature",
-		glue = {"com.emicalculator.hooks","com.emicalculator.stepDefinitions"}
+		glue = {"com.emicalculator.hooks","com.emicalculator.stepDefinitions"},
+		
+		plugin = {
+				"pretty",
+				"html:target/cucumber-reports"
+		},
+		
+		monochrome = true
 		
 		)
 public class TestNGRunner extends AbstractTestNGCucumberTests{
